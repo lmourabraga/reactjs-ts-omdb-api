89d5ab9da6d082e6464776313e1770ec4d9b7f0d
Luã Braga <ldemourabraga@gmail.com>
2020-09-04 09:21:08 -0300

    Fixing types infoMovie object

--- store/modules/infoMovie/types.ts
+++ store/modules/infoMovie/types.ts
@@ -3,8 +3,28 @@ import * as actions from './actions';
 
 export type InfoMovieAction = ActionType<typeof actions>;
 
+interface infoMovieProps {
+    Type: string,
+    Title: string,
+    Poster: string,
+    Plot: string,
+    Genre: string,
+    Director: string,
+    Writer: string,
+    Actors: string,
+    Awards: string,
+    Year: string,
+    Released: string,
+    Language: string,
+    Country: string,
+    imdbRating: string,
+    imdbVotes: string,
+    Rated: string,
+    Metascore: string,
+}
+
 export interface InfoMovieState {
     readonly loadingInfoMovieRequest: boolean;
     readonly error: boolean;
-    readonly info: [] | null;
+    readonly info: infoMovieProps | null;
 }
