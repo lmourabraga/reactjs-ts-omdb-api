edf8f0811f6cc35a6729eac5de76dac4068bbaa2
Luã Braga <ldemourabraga@gmail.com>
2020-09-04 00:01:18 -0300

    Typing redux

--- store/modules/infoMovie/types.ts
+++ store/modules/infoMovie/types.ts
@@ -6,5 +6,23 @@ export type InfoMovieAction = ActionType<typeof actions>;
 export interface InfoMovieState {
     readonly loadingInfoMovieRequest: boolean;
     readonly error: boolean;
-    readonly info: {} | null;
+    readonly info: {
+        Type: string,
+        Title: string,
+        Poster: string,
+        Plot: string,
+        Genre: string,
+        Director: string,
+        Writer: string,
+        Actors: string,
+        Awards: string,
+        Year: string,
+        Released: string,
+        Language: string,
+        Country: string,
+        imdbRating: string,
+        imdbVotes: string,
+        Rated: string,
+        Metascore: string,
+    } | null;
 }
